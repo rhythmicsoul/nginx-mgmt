972814c6505d358c830b0526601ea5cba8a5e0cd
rhythmic <rhythmic@trouble.rhythmicsoul>
2022-09-14 15:51:23 +0545

    added the TLS Support

--- client/main.go
+++ client/main.go
@@ -2,22 +2,53 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"time"
 
 	"github.com/rhythmicsoul/nginx-mgmt/proto/controller"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
+func loadTLSCreds() (credentials.TransportCredentials, error) {
+	caCert, err := ioutil.ReadFile("/home/rhythmic/certs/ca/rootCA.crt")
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(caCert) {
+		return nil, fmt.Errorf("failed to add custom ca cert")
+	}
+
+	config := &tls.Config{
+		RootCAs: certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
 func main() {
-	conn, err := grpc.Dial("localhost:9091", grpc.WithInsecure())
+	tlsCreds, err := loadTLSCreds()
+	if err != nil {
+		log.Fatalf("Could not load tls config/ca certs. Error: %v", err)
+	}
+	dctx, dcancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer dcancel()
+
+	conn, err := grpc.DialContext(dctx, "mbishnu.com.np:9091", grpc.WithTransportCredentials(tlsCreds), grpc.WithBlock(), grpc.WithReturnConnectionError())
+
 	if err != nil {
 		log.Fatalf("Cannot Connect to Server: %v", err)
 	}
 	defer conn.Close()
 
 	c := controller.NewAddServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	r, _ := c.NewAgentToken(ctx, &controller.Empty{})
--- controller/main.go
+++ controller/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"log"
 	"net"
 
 	"github.com/rhythmicsoul/nginx-mgmt/proto/controller"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type server struct {
 	controller.UnimplementedAddServiceServer
 }
 
+func loadTLSCreds() (credentials.TransportCredentials, error) {
+	serverCert, err := tls.LoadX509KeyPair("/home/rhythmic/certs/mbishnu.com/fullchain.pem",
+		"/home/rhythmic/certs/mbishnu.com/key.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	config := &tls.Config{
+		Certificates: []tls.Certificate{serverCert},
+		ClientAuth:   tls.NoClientCert,
+	}
+
+	return credentials.NewTLS(config), nil
+}
+
 func (s *server) NewAgentToken(ctx context.Context, req *controller.Empty) (*controller.AgentToken, error) {
+	log.Printf("est")
 	return &controller.AgentToken{Token: "an agent token"}, nil
 }
 
@@ -23,7 +41,13 @@ func main() {
 		log.Fatalf("Starting of server failed: %v", err)
 	}
 
-	s := grpc.NewServer()
+	tlsCreds, err := loadTLSCreds()
+	if err != nil {
+		log.Fatalf("Couldn't load the certs. Error: %v", err)
+	}
+
+	s := grpc.NewServer(grpc.Creds(tlsCreds))
+	// s := grpc.NewServer()
 	controller.RegisterAddServiceServer(s, &server{})
 	log.Printf("server listening at: %v", lis.Addr())
 
